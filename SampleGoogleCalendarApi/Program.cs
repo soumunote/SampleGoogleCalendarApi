e9680eab00038116e5700c2fe1a71d90d600d783
Note Soumu <soumunote@gmail.com>
2020-12-23 21:56:42 +0900

    Add project files.

---
+++ SampleGoogleCalendarApi/Program.cs
@@ -0,0 +1,17 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using System;
+
+namespace SampleGoogleCalendarApi
+{
+    class Program
+    {
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+            using (var stream = new FileStream)
+        }
+    }
+}
