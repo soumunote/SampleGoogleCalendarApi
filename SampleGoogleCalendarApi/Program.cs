ebcc3e5ffcdb470ef20d6522416f232e5dc7c6a7
Note Soumu <soumunote@gmail.com>
2020-12-23 23:03:05 +0900

    とりあえず、credential取得できた

--- SampleGoogleCalendarApi/Program.cs
+++ SampleGoogleCalendarApi/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Calendar.v3;
+using Google.Apis.Util.Store;
 using System;
+using System.IO;
+using System.Text;
+using System.Threading;
 
 namespace SampleGoogleCalendarApi
 {
@@ -11,7 +15,18 @@ namespace SampleGoogleCalendarApi
         static void Main(string[] args)
         {
             UserCredential credential;
-            using (var stream = new FileStream)
+
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(Properties.Resources.CREDENTIAL)))
+            {
+                var credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
         }
     }
 }
